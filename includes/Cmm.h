e141a22800db7ed5b13b12c09d837bdbff372106
Edward Z. Yang <ezyang@mit.edu>
2013-08-26 15:23:15 -0700

    Rename _closure to _static_closure, apply naming consistently.
    In preparation for indirecting all references to closures,
    we rename _closure to _static_closure to ensure any old code
    will get an undefined symbol error.  In order to reference
    a closure foobar_closure (which is now undefined), you should instead
    use STATIC_CLOSURE(foobar).  For convenience, a number of these
    old identifiers are macro'd.
    
    Across C-- and C (Windows and otherwise), there were differing
    conventions on whether or not foobar_closure or &foobar_closure
    was the address of the closure.  Now, all foobar_closure references
    are addresses, and no & is necessary.
    
    CHARLIKE/INTLIKE were not changed, simply alpha-renamed.
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- includes/Cmm.h
+++ includes/Cmm.h
@@ -321,6 +321,7 @@
    -------------------------------------------------------------------------- */
 
 #include "rts/Constants.h"
+#include "rts/StaticClosures.h"
 #include "DerivedConstants.h"
 #include "rts/storage/ClosureTypes.h"
 #include "rts/storage/FunTypes.h"
