ae572425deab2993c421182a4b3afd6338d7cca4
Edward Z. Yang <ezyang@mit.edu>
2013-08-26 15:31:42 -0700

    Implement closure indirections.
    Closure indirections live in the staticclosureinds section
    and initially point to their corresponding closure in
    the staticclosures.  The intent is that all code goes through
    a closure indirection, and at runtime the indirections can
    change.
    
    These pieces of code have to go through closure indirections:
    
        - Code references to a static closure
        - SRTs
        - Heap check (but we want to fix this)
        - References to closures in RTS (see STATIC_CLOSURE) macro
        - Runtime symbol lookup (e.g. GHCi, dynwrapper)
        - The main closure passed to hs_main
    
    Closure indirections should simplify these components (but that
    is not implemented yet):
    
        - Windows DLL handling (which already needed an indirection)
    
    In order to keep the code running, the fields of static closures
    are not indirected (although they should be); this will be switched
    over in a later commit.
    
    Important invariant: an indirection may only be dereferenced
    after the runtime is setup.  In particular, references to
    closures in __attribute__(constructor) need to be kept indirections;
    this is what the stable pointer deferral is for.
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- compiler/nativeGen/X86/Ppr.hs
+++ compiler/nativeGen/X86/Ppr.hs
@@ -387,6 +387,7 @@ pprSectionHeader seg
            UninitialisedData       -> ptext (sLit ".data\n\t.align 2")
            ReadOnlyData16          -> ptext (sLit ".const\n.align 4")
            StaticClosures          -> ptext (sLit ".section staticclosures,\"aw\"\n\t.align 2")
+           StaticClosureInds       -> ptext (sLit ".section staticclosureinds,\"aw\"\n\t.align 2")
            OtherSection _          -> panic "X86.Ppr.pprSectionHeader: unknown section"
     | otherwise ->
        case seg of
@@ -397,6 +398,7 @@ pprSectionHeader seg
            UninitialisedData       -> ptext (sLit ".data\n\t.align 3")
            ReadOnlyData16          -> ptext (sLit ".const\n.align 4")
            StaticClosures          -> ptext (sLit ".section staticclosures,\"aw\"\n\t.align 3")
+           StaticClosureInds       -> ptext (sLit ".section staticclosureinds,\"aw\"\n\t.align 3")
            OtherSection _          -> panic "PprMach.pprSectionHeader: unknown section"
    _
     | target32Bit platform ->
@@ -408,6 +410,7 @@ pprSectionHeader seg
            UninitialisedData       -> ptext (sLit ".section .bss\n\t.align 4")
            ReadOnlyData16          -> ptext (sLit ".section .rodata\n\t.align 16")
            StaticClosures          -> ptext (sLit ".section staticclosures,\"aw\"\n\t.align 4")
+           StaticClosureInds       -> ptext (sLit ".section staticclosureinds,\"aw\"\n\t.align 4")
            OtherSection _          -> panic "X86.Ppr.pprSectionHeader: unknown section"
     | otherwise ->
        case seg of
@@ -418,6 +421,7 @@ pprSectionHeader seg
            UninitialisedData       -> ptext (sLit ".section .bss\n\t.align 8")
            ReadOnlyData16          -> ptext (sLit ".section .rodata.cst16\n\t.align 16")
            StaticClosures          -> ptext (sLit ".section staticclosures,\"aw\"\n\t.align 8")
+           StaticClosureInds       -> ptext (sLit ".section staticclosureinds,\"aw\"\n\t.align 8")
            OtherSection _          -> panic "PprMach.pprSectionHeader: unknown section"
 
 
