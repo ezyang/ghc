6214ee9861625283f414fe7dbf2c53b82466f6a2
Edward Z. Yang <ezyang@mit.edu>
2013-08-27 16:52:06 -0700

    Implement runtime setup code for static closures.
    Fields of static closures are now indirected, and we have a simple
    runtime initialization code initStaticClosures() which merely rewrites
    all of the indirections into untagged pointers and untangles references
    between the static closures.  This also copies static closures into
    blocks, although this data is unused.
    
    The runtime system finds out about indirections by way of
    __attribute__(constructor) methods; thus C-- compilation may now
    generate foreign stubs (if it defines closures).
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- rts/Linker.c
+++ rts/Linker.c
@@ -30,6 +30,7 @@
 #include "Proftimer.h"
 #include "GetEnv.h"
 #include "Stable.h"
+#include "StaticClosures.h"
 
 #if !defined(mingw32_HOST_OS)
 #include "posix/Signals.h"
@@ -1049,6 +1050,8 @@ typedef struct _RtsSymbolVal {
 #define RTS_SYMBOLS                                                     \
       Maybe_Stable_Names                                                \
       RTS_TICKY_SYMBOLS                                                 \
+      SymI_HasProto(SCI_LIST)                                           \
+      SymI_HasProto(SP_LIST)                                           \
       SymI_HasProto(StgReturn)                                          \
       SymI_HasProto(stg_gc_noregs)                                      \
       SymI_HasProto(stg_ret_v_info)                                     \
@@ -1792,10 +1795,11 @@ internal_dlopen(const char *dll_name)
    o_so->next   = openedSOs;
    openedSOs    = o_so;
 
-   processPendingStablePtrs();
-
    RELEASE_LOCK(&dl_mutex);
    //--------------- End critical section -------------------
+
+   processStaticClosures();
+   processPendingStablePtrs();
 
    return errmsg;
 }
@@ -2909,6 +2913,10 @@ resolveObjs( void )
             oc->status = OBJECT_RESOLVED;
         }
     }
+
+    processStaticClosures();
+    processPendingStablePtrs();
+
     IF_DEBUG(linker, debugBelch("resolveObjs: done\n"));
     return 1;
 }
