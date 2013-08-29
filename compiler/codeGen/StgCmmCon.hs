b5088c90a6743d7489f35165a99e20fa434e17b9
Edward Z. Yang <ezyang@mit.edu>
2013-08-29 10:20:14 -0700

    CHARLIKE/INTLIKE must use indirections.
    While these are "closures", they aren't allocated the usual
    way, so we just special case them in closure initialization.
    
    It is very important that these arrays fit in a single block,
    since a pointer to a closure across the boundary needs to have
    the appropriate BF_STATIC flag set.
    
    Nota bene: C will implicitly dereference the _ind reference,
    but C-- will not; thus the extra load in C-- land.
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- compiler/codeGen/StgCmmCon.hs
+++ compiler/codeGen/StgCmmCon.hs
@@ -191,8 +191,7 @@ buildDynCon' dflags platform binder _ _cc con [arg]
               val_int = fromIntegral val :: Int
               offsetW = (val_int - mIN_INTLIKE dflags) * (fixedHdrSize dflags + 1)
                 -- INTLIKE closures consist of a header and one word payload
-              intlike_amode = cmmLabelOffW dflags intlike_lbl offsetW
-        ; return ( litIdInfo dflags binder (mkConLFInfo con) intlike_amode
+        ; return ( intCharlikeIdInfo dflags binder (mkConLFInfo con) intlike_lbl offsetW
                  , return mkNop) }
 
 buildDynCon' dflags platform binder _ _cc con [arg]
@@ -204,9 +203,8 @@ buildDynCon' dflags platform binder _ _cc con [arg]
   , val_int >= mIN_CHARLIKE dflags
   = do  { let charlike_lbl   = mkCmmClosureLabel rtsPackageId (fsLit "stg_CHARLIKE")
               offsetW = (val_int - mIN_CHARLIKE dflags) * (fixedHdrSize dflags + 1)
-                -- CHARLIKE closures consist of a header and one word payload
-              charlike_amode = cmmLabelOffW dflags charlike_lbl offsetW
-        ; return ( litIdInfo dflags binder (mkConLFInfo con) charlike_amode
+              -- CHARLIKE closures consist of a header and one word payload
+        ; return ( intCharlikeIdInfo dflags binder (mkConLFInfo con) charlike_lbl offsetW
                  , return mkNop) }
 
 -------- buildDynCon': the general case -----------
