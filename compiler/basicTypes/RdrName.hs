167df2547736675de70ebaadc002400dd5f5d53c
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-03-27 22:17:08 -0700

    Revert "Skeevy fix, UNDO ME SOON"
    This reverts commit da9afdb245c8d7620c92a940e50d86200e59de39.

--- compiler/basicTypes/RdrName.hs
+++ compiler/basicTypes/RdrName.hs
@@ -817,8 +817,7 @@ pickQualGRE mod gre@(GRE { gre_name = n, gre_lcl = lcl, gre_imp = iss })
   | otherwise           = Just (gre { gre_lcl = lcl', gre_imp = iss' })
   where
     iss' = filter (qualSpecOK mod) iss
-    -- TODO: HELLA SKEEVY but it makes module reexport work
-    lcl' = lcl
+    lcl' = lcl && name_is_from mod n
 
     name_is_from :: ModuleName -> Name -> Bool
     name_is_from mod name = case nameModule_maybe name of
